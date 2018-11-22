08a7599ce82a16c828fe7853f34ebaa7ed9896ae
Craig Adey <craig.adey@sepa.org.uk>
2018-11-22 08:51:43 +0000

    sonar qube properties file added
    some iffy code added to trigger code smells

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(){};
+
+    a() {
+    if ('a' == 'a')
+    {
+      console.log('hello');
+    }
+  }
 }
